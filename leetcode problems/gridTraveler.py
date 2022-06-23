b8b18121828b4d89e930983bdfc6887928b12342
Thomas Trenholme <thomas.trenholme@gmail.com>
2022-06-23 08:01:58 -0700

    6 22 afternoon problems
    Grind75 and some DP freecodeCamp course problems. Memoization - 1st step: brute force, find the recursion. 2nd step: make it efficient

---
+++ leetcode problems/gridTraveler.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from matplotlib.pyplot import grid
+
+
+def gridTravel(m, n):
+    numWays=0
+    def dfs(i, j):
+        if 0<=i<=m-1 and 0<=j<=n-1:
+
+            if i==m-1 and j==n-1:
+                nonlocal numWays 
+                numWays+=1
+                return
+            
+            dfs(i+1, j)
+            dfs(i, j+1)
+      
+    dfs(0, 0)
+    return numWays
+
+print(datetime.now())
+print(gridTravel(15, 15)) 
+print(datetime.now())
+
+
+def gridTravel2(m ,n, memo = {}):
+
+    if (m, n) in memo: return memo[(m, n)]
+
+    if m==0 or n==0:
+        return 0
+    if m<=1 or n<=1:
+        return 1
+    memo[(m, n)] = gridTravel2(m - 1, n, memo) + gridTravel2(m, n-1, memo)
+    return memo[(m, n)]
+
+
+print("#2: \n\n")
+print(datetime.now())
+print(gridTravel2(15, 15))
+print(datetime.now())
+
+        
