5bcd6f73dc532a726134f6a141ccfd51e1623942
Thomas Trenholme <thomas.trenholme@gmail.com>
2022-06-29 10:25:52 -0700

    leetcode 6 28 and 6 29
    grind 75 48 and 49

--- grind75/48 accountsMerge.py
+++ grind75/48 accountsMerge.py
@@ -1,11 +1,53 @@
+from collections import defaultdict
+from re import A
 from typing import List
 
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         
-        ##The first name in 
-        accountToName = {}
+        emailToAccountID = defaultdict(list)
+        accountIDtoName = {}
 
-        ##If 
-        duplicateToOrig = {}
+        for i, arr in enumerate(accounts):
+            accountIDtoName[i]=arr[0]
+
+            for email in arr[1:]:
+                emailToAccountID[email].append(i)
+
+        ##DFS from all emails in an account to all accounts (indexes) they appear in in the accounts list[list]
+        ##For all accounts in that list, DFS from them and add all emails together. Append all these values to a single array.
+
+        def dfs(accountIdx, res =[]):
+
+            if accountIdx not in visited:
+                visited.add(accountIdx)
+                for email in accounts[accountIdx][1:]:
+
+                    if email not in emailChecked:
+                        emailChecked.add(email)
+                        res.append(email)
+                        
+                        for idx in emailToAccountID[email]:
+                            moreAccounts = dfs(idx, [])
+                            if moreAccounts:
+                                res+=moreAccounts
+                        
+            return sorted(res)
+        
+        result = []
+        visited = set()
+        emailChecked=set()
+        for account in accountIDtoName:
+            dfsRes = dfs(account, [])
+            if dfsRes:
+                result.append([accountIDtoName[account]] + dfsRes)
+        
+        return result
+
+
+g = Solution()
+
+print("\n\n\n")
+print(g.accountsMerge([["David","David0@m.co","David4@m.co","David3@m.co"],["David","David5@m.co","David5@m.co","David0@m.co"],["David","David1@m.co","David4@m.co","David0@m.co"],["David","David0@m.co","David1@m.co","David3@m.co"],["David","David4@m.co","David1@m.co","David3@m.co"]]))
+            
